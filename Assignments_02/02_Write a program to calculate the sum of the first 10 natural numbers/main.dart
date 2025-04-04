c8897c8ef3f6180a765f2cac052c471575a01774
Syed Izhan Hassan <163643610+IzhanHassan007@users.noreply.github.com>
2025-04-04 18:47:30 +0500

    Delete Assignments_02 directory

--- Assignments_02/02_Write a program to calculate the sum of the first 10 natural numbers/main.dart
+++
@@ -1,9 +0,0 @@
-// Write a program to calculate the sum of the first 10 natural numbers.
-
-void main(){
-  int sum = 0;
-  for (int i = 1; i <= 10; i++) {
-    sum = sum + i;
-  }
-  print(sum);
-}
--- Assignments_02/10_Write a  program to find the frequency of each digit in a given integer/main.dart
+++
@@ -1,5 +0,0 @@
-// Write a  program to find the frequency of each digit in a given integer.
-
-void main(){
-  
-}
